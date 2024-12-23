4b1c98015e6e9c7f1abe28479b7de4ab7791edd9
EC2 Default User <ec2-user@ip-172-31-26-242.eu-north-1.compute.internal>
2024-12-23 13:24:48 +0000

    アクティブストレージ

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+
+   # nameカラムのバリデーションを追加
+  validates :name, presence: true
+  
+  enum role: { default: 0, general: 1 }
 end
--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,9 @@ module FitConnect
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = "Tokyo"
     # config.eager_load_paths << Rails.root.join("extras")
+    # デフォルトを日本語に設定
+    config.i18n.default_locale = :ja
   end
 end
